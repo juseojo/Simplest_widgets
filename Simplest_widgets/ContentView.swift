94917fa2fe0294d5bae92e4f056a97475f55fc29
juseojo <j86371224@gmail.com>
2024-11-14 17:01:33 +0900

    init

--- Simplest_widgets/ContentView.swift
+++ Simplest_widgets/ContentView.swift
@@ -11,11 +11,15 @@ struct ContentView: View {
     var body: some View {
         VStack {
 			Text("Simplest Widgets")
+				.font(.largeTitle)
+				.fontWeight(.bold)
+				.padding(.bottom)
 			Text("Go to home screen\n and add widget")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-
+				.font(.title2)
+				.fontWeight(.semibold)
+				.foregroundColor(Color.gray)
+				.multilineTextAlignment(.center)
+			Spacer()
         }
         .padding()
     }
