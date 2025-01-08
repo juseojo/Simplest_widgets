0753b47f96873d6da56d37880e12f6bdc839b39f
juseojo <j86371224@gmail.com>
2025-01-08 18:44:37 +0900

    + TemperatureBar at app

--- Simplest_widgets/TemperatureBar_view.swift
+++ Simplest_widgets/TemperatureBar_view.swift
@@ -48,44 +48,32 @@ struct TemperatureBar_view: View {
 							// VStack for 2 X 2 widget buttons
 							VStack(spacing: 0) {
 								HStack(spacing: 0) {
-									Widget_button(title: "1", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("11", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "11", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 									.padding(.trailing, widget_inform.trail_padding * ratio_num)
 
-									Widget_button(title: "2", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("12", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "12", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								}
 								.padding(.top, ratio_num * bazel.top_paddings + widget_inform.top_padding * ratio_num)
 
 								HStack(spacing: 0) {
-									Widget_button(title: "3", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("13", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "13", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 									.padding(.trailing, widget_inform.trail_padding * ratio_num)
 
-									Widget_button(title: "4", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("14", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "14", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								}
 								.padding(.top, ratio_num * widget_inform.bottom_padding)
 
 								HStack(spacing: 0) {
-									Widget_button(title: "5", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("15", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "15", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 									.padding(.trailing, widget_inform.trail_padding * ratio_num)
 
-									Widget_button(title: "6", width: widget_length, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("16", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
+									widgetItem(position: "16", widget_length: widget_length, widget_height: widget_length,
+											   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								}
 								.padding(.top, ratio_num * widget_inform.bottom_padding)
 								Spacer()
@@ -94,28 +82,16 @@ struct TemperatureBar_view: View {
 						else {
 							// VStack for 2 X 4 widget buttons
 							VStack(spacing: 0) {
-								HStack(spacing: 0) {
-									Widget_button(title: "1", width: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("21", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
-								}
+								widgetItem(position: "21", widget_length: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, widget_height: widget_length,
+										   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								.padding(.top, ratio_num * bazel.top_paddings + widget_inform.top_padding * ratio_num)
 
-								HStack(spacing: 0) {
-									Widget_button(title: "2", width: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("22", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
-								}
+								widgetItem(position: "22", widget_length: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, widget_height: widget_length,
+										   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								.padding(.top, ratio_num * widget_inform.bottom_padding)
 
-								HStack(spacing: 0) {
-									Widget_button(title: "3", width: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, height: widget_length, radius: CGFloat(widget_inform.radius) * ratio_num) {
-										UserDefaults.shared.set("23", forKey: "temperature widget position")
-										WidgetCenter.shared.reloadAllTimelines()
-									}
-								}
+								widgetItem(position: "23", widget_length: widget_length * 2.0 + widget_inform.trail_padding * ratio_num, widget_height: widget_length,
+										   widget_radius: CGFloat(widget_inform.radius), ratio_num: ratio_num)
 								.padding(.top, ratio_num * widget_inform.bottom_padding)
 								Spacer()
 							}
@@ -219,6 +195,19 @@ struct TemperatureBar_view: View {
 			}
 		}
 	}
+
+	@ViewBuilder
+	private func widgetItem(position: String, widget_length: CGFloat, widget_height: CGFloat, widget_radius: CGFloat, ratio_num: CGFloat) -> some View {
+
+		if selected_widget_position == position {
+			TemperatureBar(width: widget_length, height: widget_height)
+		} else {
+			Widget_button(title: String(position.last!), width: widget_length, height: widget_height, radius: widget_radius * ratio_num) {
+				selected_widget_position = position
+				WidgetCenter.shared.reloadAllTimelines()
+			}
+		}
+	}
 }
 
 struct Widget_button: View {
@@ -240,6 +229,55 @@ struct Widget_button: View {
 			RoundedRectangle(cornerRadius: radius)
 				.stroke(Color.white, lineWidth: 2)
 		)
+	}
+}
+
+struct TemperatureBar: View {
+	let width: CGFloat
+	let height: CGFloat
+	let position = UserDefaults.shared.string(forKey: "temperature position") ?? "1"
+
+	var body: some View {
+		ZStack {
+			if UserDefaults.shared.string(forKey: "temperature type") ?? "Horizon" == "Horizon"
+			{
+				VStack {
+					if position == "1" || position == "2" {
+						Spacer()
+					}
+
+					Rectangle()
+						.fill(Color.red)
+						.frame(width: width, height: 10)
+						.cornerRadius(30)
+						.padding(.horizontal, 10)
+
+					if position == "3" || position == "2" {
+						Spacer()
+					}
+				}
+			}
+			else
+			{
+				HStack {
+					if position == "3" || position == "2" {
+						Spacer()
+					}
+
+					Rectangle()
+						.fill(Color.blue)
+						.frame(width: 10, height: height)
+						.cornerRadius(30)
+						.padding(.horizontal, 10)
+
+					if position == "1" || position == "2" {
+						Spacer()
+					}
+				}
+			}
+		}
+		.frame(width: width, height: height)
+		.cornerRadius(30)
 	}
 }
 
