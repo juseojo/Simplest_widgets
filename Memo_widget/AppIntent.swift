bd29e67102f2fcd9f1236e8433e526d57879797d
juseojo <j86371224@gmail.com>
2025-01-14 21:18:47 +0900

    + Colors + Memo

---
+++ Memo_widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Memo_widget
+//
+//  Created by seongjun cho on 1/9/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Memo_widget/Memo_widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Memo_widgetBundle.swift
+//  Memo_widget
+//
+//  Created by seongjun cho on 1/9/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Memo_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        Memo_widget()
+    }
+}
